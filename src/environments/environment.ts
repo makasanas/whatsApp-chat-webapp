a57fdcae8130022ef022daeaf0b89b2ea3eb6997
Dako patel <makasanadarshit092@gmail.com>
2020-11-06 16:46:01 +0530

    New Updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  appId: "0c436ac2893fdf35e9a4d8924f6c5aaa",
-  appScope: "read_products,write_products,read_product_listings",
+  appId: "69d905edbaf07871f7ba034521f9dd26",
+  appScope: "read_products,write_products,read_product_listings,read_script_tags, write_script_tags,read_themes,write_themes",
   apiUrl: "http://localhost:3000/",
   store: "http://dev-srore.myshopify.com",
   crispId: "e709e725-c026-4052-a8f4-377bb9b15f96",
